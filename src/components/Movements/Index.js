315a0a6cdfcb332dd89af843ebd8272d06f2235e
gama506 <joaovictorgamaramos@gmail.com>
2025-07-01 11:17:02 -0300

    Atualizado

--- src/components/Movements/Index.js
+++ src/components/Movements/Index.js
@@ -1,5 +1,32 @@
-export default function Movements () {
-       return ( 
-          <Text>Componente Movements</Text>
-       )
+import { useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View } from "react-native-web";
+export default function Movements({ dados }) {
+  const [mostrarValor, setMostrarValor] = useState(false);
+
+  // CRIANDO UAM FUNÇÃO PARA TRANSFORMAR EM MOEDA
+  function transformarEmMoeda(dinheiro) {
+    return dinheiro.tolocalString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
+
+  <TouchableOpacity style={styles.container}>
+    <Text style={styles.data}>{dados.data}</Text>
+
+    <Text style={styles.conteudo}>
+      {dados.titulo}
+
+      {mostrarValor ? (
+      <Text style={styles.tipo === 1 ? styles.valor : styles.despesas
+      <Text style={styles.transformarEmMoeda(dados.valor)${}`}>
+      }
+        </Text>
+      ) : (
+        <View></View>
+      )}
+  </TouchableOpacity>;
 }
+
+const styles = StyleSheet.create[{}];
--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,15 +1,101 @@
 import { View } from "react-native-web";
 import Header from "../../components/Header/Index";
 import Balance from "../../components/Balance/Index";
+import { StyleSheet } from "react-native";
+const lista = [
+  {
+    id: 1,
+    titulo: "Boleto de luz",
+    valor: 230,
+    data: "23/06/2025",
+    tipo: 0,
+  },
+  {
+    id: 2,
+    titulo: "Boleto de agua",
+    valor: 139,
+    data: "23/06/2025",
+    tipo: 0,
+  },
 
+  {
+    id: 3,
+    titulo: "Conta de internet",
+    valor: 290,
+    data: "23/06/2025",
+    tipo: 0,
+  },
+
+  {
+    id: 4,
+    titulo: "Boleto de luz",
+    valor: 230,
+    data: "23/06/2025",
+    tipo: 0,
+  },
+
+  {
+    id: 5,
+    titulo: "dizimo",
+    valor: 40,
+    data: "23/06/2025",
+    tipo: 0,
+  },
+
+  {
+    id: 6,
+    titulo: "aluguel",
+    valor: 500,
+    data: "23/06/2025",
+    tipo: 0,
+  },
+
+  {
+    id: 7,
+    titulo: "transferência de nicolas",
+    valor: 100,
+    data: "23/06/2025",
+    tipo: 1,
+  },
+
+  {
+    id: 8,
+    titulo: "transferência de sofia",
+    valor: 300,
+    data: "23/06/2025",
+    tipo: 1,
+  },
+
+  {
+    id: 9,
+    titulo: "transferência de sophia",
+    valor: 900,
+    data: "23/06/2025",
+    tipo: 1,
+  },
+
+  {
+    id: 10,
+    titulo: "transferência de ana julia",
+    valor: 1000,
+    data: "23/06/2025",
+    tipo: 1,
+  },
+];
 export default function Home() {
-    return (
-        <View>
-            <Header />
+  return (
+    <View style={style.container}>
+      <Header />
 
-            <Balance saldo={"R$ 1200,00"} gastos={"R$ 600,00"} />
+      <Balance saldo={"R$ 1200,00"} gastos={"R$ 600,00"} />
 
-            <Action />
-        </View>
-    )
+      <Action />
+
+      <Text style={StyleSheet.titulo}>Últimas movimentações</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create[{
+    
+}]
